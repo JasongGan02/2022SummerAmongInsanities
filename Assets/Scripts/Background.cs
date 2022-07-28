17a75ed074fd40e564af20d3190e6ae4760cf264
KEVqwe <109773270+KEVqwe@users.noreply.github.com>
2022-07-28 01:55:18 -0400

    Add files via upload

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float length,startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dis = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + dis,transform.position.y,transform.position.z);
+
+        if (temp > (startpos + length))
+        {
+            startpos += length;
+        }
+        else if (temp < (startpos - length))
+        { 
+            startpos -= length;
+        }
+                                                                            
+    }
+ 
+}
