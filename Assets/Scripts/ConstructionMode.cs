803a2a0abd541313b90ee90f7a287b481167da0e
EasyMood06 <xuqikun0306@gmail.com>
2022-08-04 23:51:20 -0700

    Modifying the construction mode

--- Assets/Scripts/ConstructionMode.cs
+++ Assets/Scripts/ConstructionMode.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ConstructionMode : MonoBehaviour
 {
     private bool isInConstructionMode = false;
-
+    private Constants.TowerType towerType = Constants.TowerType.noShadow;
+    private GameObject shadowObj;
     
     [SerializeField] List<GameObject> Towers;
     [SerializeField] GameObject ConstructionUI;
@@ -31,7 +32,30 @@ public class ConstructionMode : MonoBehaviour
         {
             ExitConstruction();
         }
+        
+        if(towerType != Constants.TowerType.noShadow){
+            Vector2 rayOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D downRay = Physics2D.Raycast(rayOrigin, Vector2.down, 100.0f, 1 << Constants.Layer.GROUND);
+            bool regenerate = false;
+            if(shadowObj){
+                char objIdx = shadowObj.name[shadowObj.name.Length-8];
+                regenerate = (int)towerType != ((int)char.GetNumericValue(objIdx)-1);
+            }
+            if(!shadowObj || regenerate)
+            {
+                if(shadowObj){
+                    Destroy(shadowObj);
+                }
+                shadowObj = Instantiate(Towers[(int)towerType]);
+                shadowObj.GetComponent<BoxCollider2D>().isTrigger = true;
+                shadowObj.layer = Constants.Layer.DEFAULT;
+            }
 
+            if(shadowObj && downRay){
+                shadowObj.transform.position = downRay.transform.position;
+                shadowObj.transform.position += new Vector3(0, shadowObj.GetComponent<BoxCollider2D>().size.y/2, 0);
+            }
+        }
     }
 
     void EnterConstruction()
@@ -49,35 +73,27 @@ public class ConstructionMode : MonoBehaviour
         // hide the construction UI
         ConstructionUI.SetActive(false);
         // hide the image in under the cursor
+        towerType = Constants.TowerType.noShadow;
 
     }
 
     void ShowConstructionUnderCursor()
     {
-        Vector3 mousePositionInMinusTen = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 mousePosition = new Vector3(mousePositionInMinusTen.x, mousePositionInMinusTen.y, 0);
-        GeneratingTower(mousePosition);
-        
-
-    }
-    
-    void GeneratingTower(Vector3 generatingPosition)
-    {
         if(Input.GetKeyUp(KeyCode.Alpha1))
         {
-            Instantiate(Towers[0],generatingPosition,Quaternion.identity);
+            towerType = Constants.TowerType.typeOne;
         }
         if(Input.GetKeyUp(KeyCode.Alpha2))
         {
-            Instantiate(Towers[1],generatingPosition,Quaternion.identity);
+            towerType = Constants.TowerType.typeTwo;
         }
         if(Input.GetKeyUp(KeyCode.Alpha3))
         {
-            Instantiate(Towers[2],generatingPosition,Quaternion.identity);
+            towerType = Constants.TowerType.typeThree;
         }
         if(Input.GetKeyUp(KeyCode.Alpha4))
         {
-            Instantiate(Towers[3],generatingPosition,Quaternion.identity);
+            towerType = Constants.TowerType.typeFour;
         }
     }
 }
