ea69311982dda866579cf676002f284f9f4fddec
JasonGan <ganjason@usc.edu>
2024-01-15 09:01:11 -0800

    RangedWeapo's projectile Object fix

--- Assets/Scripts/Characters/Weapon/Weapon/BowController.cs
+++ Assets/Scripts/Characters/Weapon/Weapon/BowController.cs
@@ -51,8 +51,6 @@ public class BowController : RangedWeaponController
 
     public override void FireProjectile(GameObject target)
     {
-        Debug.Log(projectileObject);
-
         if (!inventory.ConsumeItem(projectileObject, 1))
             return;
         // Calculate the force and damage based on charge time
