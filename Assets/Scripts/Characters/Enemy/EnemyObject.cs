094e79217e2c59e501f5717772b10f27d50ed40f
JasonGan <ganjason@usc.edu>
2024-03-17 19:08:06 -0700

    EnemySpawns

--- Assets/Scripts/Characters/Enemy/EnemyObject.cs
+++ Assets/Scripts/Characters/Enemy/EnemyObject.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 
-public class EnemyObject : CharacterObject
+public class EnemyObject : CharacterObject, ISpawnable
 {
     public float SensingRange;
-    public int 
+    //public int 
     public void LevelUp()
     {
         _HP *= 1.1f;
         _atkDamage *= 1.1f;
     }
+
+    [Header("ISpawnable Fields")] 
+    [SerializeField] float minLightLevel;
+    [SerializeField] float maxLightLevel;
+    [SerializeField] float spawnWeight;
+    [SerializeField] int groupSize;
+    [SerializeField] string biome;
+    [SerializeField] Vector2Int colliderSize;
+    
+    public float MinLightLevel => minLightLevel;
+    public float MaxLightLevel => maxLightLevel;
+    public float SpawnWeight => spawnWeight;
+    public int GroupSize => groupSize;
+    public string Biome => biome;
+    public Vector2Int ColliderSize => colliderSize;
 }
