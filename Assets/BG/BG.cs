3e98a4f2913d0c03d31b1d18bc0fba279c8e3754
Scott Tang <ruitang591@gmail.com>
2024-04-16 19:18:10 -0700

    Update BG.cs
    BG vertical parralex add

--- Assets/BG/BG.cs
+++ Assets/BG/BG.cs
@@ -1,35 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BG : MonoBehaviour
 {
-    private float length, startpos;
+    private Vector2 startPos;
+    private Vector2 travel => (Vector2)cam.transform.position - startPos;
     private GameObject cam;
     public float parallaxEffect;
 
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-        cam = Camera.main.transform.gameObject;
+        startPos = transform.position;
+        cam = Camera.main.gameObject;
     }
 
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dis = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dis, transform.position.y, transform.position.z);
-
-        if (temp > (startpos + length))
-        {
-            startpos += length;
-        }
-        else if (temp < (startpos - length))
-        {
-            startpos -= length;
-        }
-
+        Vector2 newPos = startPos + travel * parallaxEffect;
+        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
     }
-
 }
